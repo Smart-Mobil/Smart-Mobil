753da45084cca3578b24b907b0548062b35b3761
JinYeob Kim <37360089+Jinyeob@users.noreply.github.com>
2020-01-20 23:56:56 +0900

    LED 추가

--- ardu_/ardu_.ino
+++ ardu_/ardu_.ino
@@ -10,13 +10,28 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 PM2008_I2C pm2008_i2c;
 DHT dht(DHTPIN, DHTTYPE);
 
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+
 void setup() {
+ pinMode(redPin, OUTPUT);
+ pinMode(greenPin, OUTPUT);
+ pinMode(bluePin, OUTPUT); 
+ 
   pm2008_i2c.begin();
   pm2008_i2c.command();
   
   dht.begin();
   mlx.begin();  
   Serial.begin(9600);
+}
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue); 
 }
 
 void loop() {
@@ -26,6 +41,7 @@ void loop() {
    3. 체온 (비접촉식온도센서)
    */
    
+  delay(1000);
   //미세먼지
   uint8_t ret = pm2008_i2c.read();
   if (ret == 0) {
@@ -35,8 +51,9 @@ void loop() {
     Serial.print(pm2008_i2c.pm2p5_tsi);
   }
   
+  setColor(255, 0, 0); // red
+  setColor(255, 255, 0);// yellow
   Serial.println();
-  delay(1000);
   
   //온습도
   float h = dht.readHumidity();
@@ -53,14 +70,16 @@ void loop() {
     Serial.print("%");
   }
   
+  setColor(0, 255, 0); // green
+  setColor(0, 255, 255);// aqua
   Serial.println();
-  delay(1000);
   
   //비접촉식온도센서
   Serial.print("3."); 
   Serial.print(mlx.readObjectTempC()); 
   Serial.print("*C"); 
 
+  setColor(0, 0, 255); // blue
+  setColor(255, 0, 255); // blue
   Serial.println();
-  delay(1000);
 }
