9e09d73dd5579ec73988df15daedd64192b2ec87
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2020-01-22 23:55:33 +0900

    node.js1차 성공

--- ardu_/node.js
+++ ardu_/node.js
@@ -18,8 +18,10 @@ var val;
 
 serialPort.open(function () {
     serialPort.on('data',function(data){
-    val += data + '@#';  
-
+    val += data;  
+    if(val.length >= 1000000000 ){
+      val = val.substring(1,1);  
+    }
 });
 
 
@@ -31,9 +33,31 @@ app.get("/", (req, res) => {
     serialPort.on('data',function(data){
 // Pug 파일 렌더링
 app.get("/hello", (req, res) => {
-    console.log(val+ d);
+  var h1;
+  var h2;
+  var h3;
+  for(var i = val.length - 1; i>0; i--){
+    if(val[i] === '1'){
+      if(val[i+1] === ')'){
+         h1 = val.substring(i,i+8);
+      }
+    }
+    else if(val[i] === '2'){
+      if(val[i+1] === ')'){
+        h2 = val.substring(i,i+17);
+      }
+    }
+    else if(val[i] === '3'){
+      if(val[i+1] === ')'){
+        h3 = val.substring(i,i+10);
+      }
+    }
+    if( h1 && h2 && h3)
+      break;
+  }
+    console.log(h1 +'@'+ h2+ '@'+ h3+'@');
     var d = new Date();
-    res.render('hello', { title: 'Hello', message: val+d})
+    res.render('hello', { title: 'Hello',message1: h1, message2: h2, message3 : h3, message4: d  })
 })
 })
 
