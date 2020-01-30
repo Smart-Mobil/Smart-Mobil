bc31cb0e5b6011ae52647955ca97abe1378e620c
JinYeob Kim <37360089+Jinyeob@users.noreply.github.com>
2020-01-30 11:43:22 +0900

    arduino 시리얼데이터 정리

--- ardu_/ardu_.ino
+++ ardu_/ardu_.ino
@@ -14,16 +14,24 @@ int redPin = 11;
 int greenPin = 10;
 int bluePin = 9;
 
+String first="1)";
+String second="2)";
+String third="3)";
+
+String comma=",";
+String slash="/\n";
+
+String temp="*C";
+String percent="%";
+
+String allString="";
+
 void setup() {
- pinMode(redPin, OUTPUT);
- pinMode(greenPin, OUTPUT);
- pinMode(bluePin, OUTPUT); 
- 
   pm2008_i2c.begin();
   pm2008_i2c.command();
   
   dht.begin();
-  mlx.begin();  
+  mlx.begin();
   Serial.begin(9600);
 }
 
@@ -35,54 +43,38 @@ void setColor(int red, int green, int blue)
 }
 
 void loop() {
+   delay(1000);
    /* 
    1. 미세먼지
    2. 온습도
    3. 체온 (비접촉식온도센서)
    */
-   
-  delay(1000);
+   /*
+   if( Serial.available())
+  {
+    Serial.write(Serial.read());
+  }
+ */
+  
   //미세먼지
   uint8_t ret = pm2008_i2c.read();
   if (ret == 0) {
-    Serial.print("1)");
-    Serial.print(pm2008_i2c.pm10_tsi);
-    Serial.print(",");
-    Serial.print(pm2008_i2c.pm2p5_tsi);
-    Serial.print("/");
+    allString=first+String(pm2008_i2c.pm10_tsi)+comma+String(pm2008_i2c.pm2p5_tsi)+slash;
   }
-  
-  setColor(255, 0, 0); // red
-  setColor(255, 255, 0);// yellow
-  Serial.println();
   
   //온습도
-  float h = dht.readHumidity();
   float t = dht.readTemperature();
-
-  if (isnan(t) || isnan(h)) {
+  float h = dht.readHumidity();
+  if (isnan(t) || isnan(h)) 
+  {
   //값 읽기 실패시
-  } else {
-    Serial.print("2)");
-    Serial.print(t);
-    Serial.print("*C");
-    Serial.print(",");
-    Serial.print(h);
-    Serial.print("%");
-    Serial.print("/");
+  } 
+  else {
+   allString+=second+String(t)+temp+comma+String(h)+percent+slash;
   }
   
-  setColor(0, 255, 0); // green
-  setColor(0, 255, 255);// aqua
-  Serial.println();
-  
   //비접촉식온도센서
-  Serial.print("3)"); 
-  Serial.print(mlx.readObjectTempC()); 
-  Serial.print("*C"); 
-  Serial.print("/");
+  allString+=third+String(mlx.readObjectTempC())+temp+slash;
 
-  setColor(0, 0, 255); // blue
-  setColor(255, 0, 255); // blue
-  Serial.println();
+  Serial.print(allString);
 }
