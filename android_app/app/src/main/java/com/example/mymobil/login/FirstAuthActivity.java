e8c72a0e962b7cfff4776acb5be6e6a45d7d32bf
JinYeob Kim <jinyeob07@gmail.com>
2020-02-22 12:47:39 +0900

    (android) login 패키지 추가

---
+++ android_app/app/src/main/java/com/example/mymobil/login/FirstAuthActivity.java
@@ -0,0 +1,36 @@
+package com.example.mymobil.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.mymobil.MainActivity;
+import com.example.mymobil.R;
+
+public class FirstAuthActivity extends AppCompatActivity {
+
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_auth);
+
+        if(SaveSharedPreference.getUserName(FirstAuthActivity.this).length() == 0 || SaveSharedPreference.getUserPasswd(FirstAuthActivity.this).length() == 0) {
+            // call Login Activity
+            intent = new Intent(FirstAuthActivity.this, LoginActivity.class);
+
+            startActivity(intent);
+            this.finish();
+        } else {
+            // Call Next Activity
+            intent = new Intent(FirstAuthActivity.this, MainActivity.class);
+            intent.putExtra("id", SaveSharedPreference.getUserName(this).toString());
+            intent.putExtra("passwd", SaveSharedPreference.getUserPasswd(this).toString());
+
+            startActivity(intent);
+            this.finish();
+        }
+    }
+}
