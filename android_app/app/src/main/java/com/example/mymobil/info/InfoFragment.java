c9baf36528f7e3aba8905aeb95e5ffef3b3539b1
Jinyeob <jinyeob07@gmail.com>
2020-03-11 20:27:26 +0900

    (android) add <baby care info video> - using Youtube api

--- android_app/app/src/main/java/com/example/mymobil/info/InfoFragment.java
+++ android_app/app/src/main/java/com/example/mymobil/info/InfoFragment.java
@@ -13,15 +13,15 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mymobil.R;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import static android.provider.MediaStore.Video.Thumbnails.VIDEO_ID;
 
 public class InfoFragment extends Fragment {
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_info, container, false);
-        final TextView textView = root.findViewById(R.id.text_info);
-
         return root;
     }
 }
