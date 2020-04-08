348f0ec386551b49b2064ca7d2914c9b6f079523
gusdnr9875 <gusdnr9875@naver.com>
2020-04-08 12:42:33 +0900

    asd

--- node.js/DB.js
+++ node.js/DB.js
@@ -1,12 +1,23 @@
 var mysql      = require('mysql');
-/*
+
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'localhost:3306/smartmobil',
   user     : 'root',
   password : '5289',
   database : 'ENVIRONMENT'
 });
 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+/*
 connection.connect();
 
 connection.query('SELECT * from s', function(err, rows, fields) {
