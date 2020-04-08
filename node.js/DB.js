4f2d267f466cc05802e1edbec1fd6680983db037
gusdnr9875 <gusdnr9875@naver.com>
2020-04-08 10:03:24 +0900

    add table query

--- node.js/DB.js
+++ node.js/DB.js
@@ -3,8 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '5289',
-  port     : 3306,
-  database : 'my_db'
+  database : 'ENVIRONMENT'
 });
 
 connection.connect();
