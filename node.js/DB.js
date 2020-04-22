1958f3faad4aeba14a748840e3d85039128b479c
gusdnr9875 <gusdnr9875@naver.com>
2020-04-22 22:22:16 +0900

    asd

--- node.js/DB.js
+++ node.js/DB.js
@@ -4,32 +4,39 @@ var connection = mysql.createConnection({
   port: "3306",
   host     : 'localhost',
   user     : 'root',
-  password : '*B3ECDE53DBC07418E558CAE7F34AD5CC4761C4F6',
-  database : 'smartmobil'
+  password : '',
+  database : 'smartmobil',
+  insecureAuth : true
 });
 
+connection.connect();
+console.log("Connected!");
 
-connection.connect(function(err) {
-  
-  console.log("Connected!");
-  var sql = "INSERT INTO ENVIRONMENT (time, tempandhum, temperature, dust ) VALUES ('test', 'test', 'test', 'test')";
-  connection.query(sql, function (err, result) {
-    console.log("1 record inserted");
+
+  var sql = "INSERT INTO ENVIRONMENT (time, tempandhum, temperature, dust ) VALUES (?,?,?,?)";
+  var params = ['test title','testuser','testpw','testcontent'];
+
+  connection.query(sql,params,function(err,rows,fields) {
+    if(err){
+      console.log(err);
+    }else{
+      console.log(rows.insertId);
+    }
   });
-});
 
-connection.query('select * from ENVIRONMENT', function (err, rows, fields) {
-  if (!err) {
-      console.log(rows);
-      console.log(fields);
-      var result = 'rows : ' + JSON.stringify(rows) + '<br><br>' +
-          'fields : ' + JSON.stringify(fields);
-      res.send(result);
-  } else {
-      console.log('query error : ' + err);
-      
+
+var sql2 = 'SELECT * FROM ENVIRONMENT';
+
+connection.query(sql2,function(err,rows,fields) {
+  if(err){
+    console.log(err);
+  }else{
+    for(var i =0;i<rows.length;i++){
+      console.log(rows[i].board_title);
+    }
   }
 });
+
 connection.end();
 
 /*
