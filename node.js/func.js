c920061a6e994e25bf6abcf19bf533a509081d21
gusdnr9875 <gusdnr9875@naver.com>
2020-04-26 20:57:14 +0900

    git

--- node.js/func.js
+++ node.js/func.js
@@ -11,11 +11,8 @@ ledRed.digitalWrite(1); // Turn RED LED off
 ledGreen.digitalWrite(1); // Turn GREEN LED off
 ledBlue.digitalWrite(1); // Turn BLUE LED off
 
-
 // 스피커기능
 var player = require('play-sound')(opts = {})
-
-
 
 // 주요 기능들에 따른 함수를 호출할 예정입니다. 
 exports.onLed = function main( argument1 ) {
