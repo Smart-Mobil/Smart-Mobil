69b9471aedffd46655183122d3b5d1a6683c591a
JinYeob Kim <37360089+Jinyeob@users.noreply.github.com>
2020-04-30 22:33:24 +0900

    (Android) Update UI Envfragment - circle progressbar

--- android_app/app/src/main/java/com/example/mymobil/SplashActivity.java
+++ android_app/app/src/main/java/com/example/mymobil/SplashActivity.java
@@ -4,19 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.TedPermission;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * Create by Jinyeob on 2020. 04. 29.
--- android_app/app/src/main/java/com/example/mymobil/env/EnvFragment.java
+++ android_app/app/src/main/java/com/example/mymobil/env/EnvFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.URLUtil;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,7 +39,17 @@ import static android.content.Context.MODE_PRIVATE;
 public class EnvFragment extends Fragment {
     private TextView textView;
     private String EnvUrl = "";
+
+    int dust_;
+    int temp_;
+    int hum_;
+    int tempBody_;
+
     private String allString = "";
+    private ProgressBar progressBar_dust;
+    private ProgressBar progressBar_temp;
+    private ProgressBar progressBar_hum;
+
 
     //Bundle extra;
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -72,6 +83,13 @@ public class EnvFragment extends Fragment {
         }
 
         textView = root.findViewById(R.id.text_env);
+        progressBar_dust = (ProgressBar)root.findViewById(R.id.cpb1);
+        progressBar_temp = (ProgressBar)root.findViewById(R.id.cpb2);
+        progressBar_hum = (ProgressBar)root.findViewById(R.id.cpb3);
+
+        progressBar_dust.setProgress(20);
+        progressBar_temp.setProgress(25);
+        progressBar_hum.setProgress(40);
 
         /* URL 검사용 조건문 */
         if (URLUtil.isValidUrl(EnvUrl)) {
@@ -79,7 +97,7 @@ public class EnvFragment extends Fragment {
                 public void run() {
                     while (true) {
                         try {
-                            Thread.sleep(1000); //1초 주기로 파싱
+                            Thread.sleep(3000); //1초 주기로 파싱
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -115,8 +133,9 @@ public class EnvFragment extends Fragment {
 
 
             if (0 < idx && idx + 1 < dustString.indexOf("/")) {
+                dust_= Integer.parseInt(dustString.substring(idx + 1, dustString.indexOf("/")));
                 dustString1 = "<미세먼지>\nPM10 : " + dustString.substring(0, idx) + "\n";
-                dustString2 = "PM2.5 : " + dustString.substring(idx + 1, dustString.indexOf("/")) + "\n\n";
+                dustString2 = "PM2.5 : " + dust_ + "\n\n";
 
                 return dustString1 + dustString2;
             } else {
@@ -137,8 +156,11 @@ public class EnvFragment extends Fragment {
             int idx2 = THString.indexOf(",");
 
             if (0 < idx2 && idx2 + 1 < THString.indexOf("/")) {
-                tempString = "<온습도>\n온도 : " + THString.substring(0, idx2) + "\n";
-                humString = "습도 : " + THString.substring(idx2 + 1, THString.indexOf("/")) + "\n\n";
+                temp_= Integer.parseInt(THString.substring(0, idx2));
+                hum_=Integer.parseInt(THString.substring(idx2 + 1, THString.indexOf("/")));
+
+                tempString = "<온습도>\n온도 : " + temp_ + "\n";
+                humString = "습도 : " + hum_ + "\n\n";
 
                 return tempString + humString;
             } else {
@@ -154,7 +176,8 @@ public class EnvFragment extends Fragment {
         if (bodyTempString.length() > 0) {
             bodyTempString = bodyTempString.substring(bodyTempString.lastIndexOf(")") + 1);
             if (0 < bodyTempString.indexOf("/")) {
-                bodyTempString = "체온 : " + bodyTempString.substring(0, bodyTempString.indexOf("/")) + "\n\n";
+                tempBody_=Integer.parseInt(bodyTempString.substring(0, bodyTempString.indexOf("/")));
+                bodyTempString = "체온 : " + tempBody_ + "\n\n";
 
                 return bodyTempString;
             } else {
